81b01bf8e86e5ec86098f0905bf1b2491f850b5e
Chandan Singh <chandan_singh@berkeley.edu>
2022-09-09 20:46:36 +0000

    cleaner classification plot

--- 05_prompt_classification.py
+++ 05_prompt_classification.py
@@ -22,21 +22,21 @@ if __name__ == '__main__':
         'max_digit': 10,
     }
     args = argparse.Namespace(**args_dict)
-    
+
     # model_name = 'EleutherAI/gpt-j-6B'  # 'EleutherAI/gpt-neox-20b'
-    # model_name = 'gpt2-medium'  # 'EleutherAI/gpt-neox-20b'
+    model_name = 'gpt2-medium'  # 'EleutherAI/gpt-neox-20b'
     # model_name = 'facebook/opt-1.3b'
-    model_name = 'EleutherAI/gpt-neox-20b'
+    # model_name = 'EleutherAI/gpt-neox-20b'
     save_dir = 'results'
     # task_names = TASKS.keys()
     task_names = ['add_two', 'divide_two', 'max_two',
                   'subtract_two', 'first_two', 'multiply_two']
     # task_names = list(set(TASKS.keys()) - {'SUFFIXES'})
     batch_size = 1
-    # device = 'cpu' 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   
+    # device = 'cpu'
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    # set up tasks    
+    # set up tasks
     print('set up tasks')
     task_information = {}
     task_descriptions = []
@@ -44,7 +44,6 @@ if __name__ == '__main__':
         dset, check_answer_func, description = get_data(
             args=args, task_name=name)
         task_descriptions.append(description)
-
 
     # load stuff
     print(f'load model {model_name}...')
@@ -61,16 +60,21 @@ if __name__ == '__main__':
         name = task_names[i]
         for j in range(len(task_descriptions)):
             description = task_descriptions[j]
-            #
             dset, check_answer_func, __this_task_description = get_data(
                 args=args, task_name=name)
             loss, acc = test_model_on_task_with_prefix(
                 dset=dset, model=model, tokenizer=tokenizer, prefix=f'{description} ', device=device)
+
+            # save things
             losses[i][j] += loss
             accuracies[i][j] += acc
             # print(f'Task: {name} \t Prefix: {description} \t Loss: {loss:.2f}')
 
     # save
     os.makedirs(save_dir, exist_ok=True)
-    pkl.dump({'losses': losses, 'accuracies': accuracies},
+    pkl.dump({
+        'task_names': task_names,
+        'task_descriptions': task_descriptions,
+        'losses': losses,
+        'accuracies': accuracies},
              open(oj(save_dir, f'heatmap_{model_name.replace("/", "___")}.pkl'), 'wb'))
