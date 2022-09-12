8c2beda596c24ef85f43161a130e1a360a5b4086
Chandan Singh <chandan_singh@berkeley.edu>
2022-09-12 04:57:24 +0000

    proper anli 10-dataset version

--- data_utils/anli.py
+++ data_utils/anli.py
@@ -24,18 +24,37 @@ TASKS_ANLI = {
     'task1149_item_check_edible': {
         'check_answer_func': r'edible|eatable|safe to eat',
     },
-    'task1149_item_check_edible': {
-        'check_answer_func': r'edible|eatable|safe to eat',
+    # 'task1321_country_continent': { # Given a word generate its antonym	
+        # 'check_answer_func': r'edible|eatable|safe to eat', 
+    # },
+    'task1509_evalution_antonyms': { # Given a word generate its antonym	
+        'check_answer_func': r'opposite|antonym', 
     },
-    'task429_senteval_tense': {
-        'check_answer_func': r'tense|past|present',
+    'task183_rhyme_generation': { # Given a word generate its antonym	
+        'check_answer_func': r'rhyme', 
     },
-    'task430_senteval_subject_count': {
-        'check_answer_func': r'singular|plural', # this is shaky
+    'task1191_food_veg_nonveg': { # Given a word generate its antonym	
+        'check_answer_func': r'vegetarian|vegan|meat', 
     },
-    'task609_sbic_potentially_offense_binary_classification': {
-        'check_answer_func': r'offensive|toxic|harmful|derogatory|hate speech', # this is shaky
+    'task092_check_prime_classification': { # Given a word generate its antonym	
+        'check_answer_func': r'prime', 
     },
+    'task107_splash_question_to_sql': { # Given a word generate its antonym	
+        'check_answer_func': r'sql', 
+    },
+    'task1336_peixian_equity_evaluation_corpus_gender_classifier': { # Given a word generate its antonym	
+        'check_answer_func': r'gender|fem|masc', 
+    },
+    # 'task429_senteval_tense': {
+        # 'check_answer_func': r'tense|past|present',
+    # },
+    # 'task430_senteval_subject_count': {
+        # 'check_answer_func': r'singular|plural', # this is shaky
+    # },
+    # 'task609_sbic_potentially_offense_binary_classification': {
+        # 'check_answer_func': r'offensive|toxic|harmful|derogatory|hate speech', # this is shaky
+    # },
+
     'SUFFIXES': ['What is the task defined above?'],
 }
 ks = list(TASKS_ANLI.keys())
