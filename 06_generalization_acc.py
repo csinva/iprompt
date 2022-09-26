119430baa2096d0236f566826e33032faffbaeca
Chandan Singh <chandan_singh@berkeley.edu>
2022-09-26 17:33:24 +0000

    posthoc compute accs for everything

--- 06_generalization_acc.py
+++ 06_generalization_acc.py
@@ -18,6 +18,12 @@ import pickle as pkl
 import os
 import dvu
 dvu.set_style()
+from os.path import dirname
+repo_dir = dirname(os.path.abspath(__file__))
+results_acc_dir = oj(repo_dir, 'results', 'generalization_acc')
+os.makedirs(results_acc_dir, exist_ok=True)
+
+
 
 
 task_names_math_one = ['square_one', 'exp_one', 'double_one', 'fibonacci_one']
@@ -31,19 +37,48 @@ task_names_anli = ['task1146_country_capital', 'task1509_evalution_antonyms', 't
                    ]
 
 
-
 ######################## ACTUAL HYPERPARAMS ################################
-checkpoints = ['EleutherAI/gpt-neo-2.7B', 'EleutherAI/gpt-j-6B']
-max_digit = 10 # If this is not 10, only math gets run!
+checkpoints_test = ['EleutherAI/gpt-neo-2.7B', 'EleutherAI/gpt-j-6B']
 TASK_SETTINGS = {
-    'one_digit_all': task_names_math_one + task_names_math_two + task_names_anli,
-    'double_digit_two_nums': task_names_math_two,
-    'one_digit_three_nums': task_names_math_three,
+    'one_digit_all': {
+        'task_names': task_names_math_one + task_names_math_two + task_names_anli,
+        'max_digit': 10,
+        'n_shots': [1, 5, 10],
+        'prompt_types': ['', 'manual'],
+    },
+    'double_digit_two_nums': {
+        'task_names': task_names_math_two,
+        'max_digit': 100,
+        'n_shots': [1, 5, 10],
+        'prompt_types': ['', 'manual'],
+    },
+    'one_digit_three_nums': {
+        'task_names': task_names_math_three,
+        'max_digit': 10,
+        'n_shots': [1, 5, 10],
+        'prompt_types': ['', 'manual'],
+    },
+    'sweep_in_distr_math': {
+        'task_names': task_names_math_one + task_names_math_two, # + task_names_anli,
+        'max_digit': 10,
+        'n_shots': [1],
+        'prompt_types': ['autoprompt', 'iprompt', '', 'manual'],  # ['', 'manual'],
+    },
+    'sweep_double_digit_math': {
+        'task_names': task_names_math_two,
+        'max_digit': 100,
+        'n_shots': [1],
+        'prompt_types': ['autoprompt', 'iprompt', '', 'manual'], 
+    },
+    'sweep_one_digit_three_nums_math': {
+        'task_names': task_names_math_three,
+        'max_digit': 10,
+        'n_shots': [1],
+        'prompt_types': ['autoprompt', 'iprompt', '', 'manual'], 
+    },
 }
-task_key = 'one_digit_three_nums'
-tasks = TASK_SETTINGS[task_key]
+task_key = 'sweep_one_digit_three_nums_math'
 ############################################################################
-
 
 
 # prepare the args
@@ -53,6 +88,8 @@ batch_sizes = {
     'EleutherAI/gpt-neo-2.7B': 16,
     'EleutherAI/gpt-neox-20b': 1,
 }
+
+
 class fake_args:
     template_num_task_phrasing = 0
     max_dset_size = 1000
@@ -67,31 +104,37 @@ class fake_args:
 
 args = fake_args()
 np.random.seed(args.seed)
+settings = TASK_SETTINGS[task_key]
+args.max_digit = settings['max_digit']
+prompts_saved = pkl.load(open(oj(results_acc_dir, 'prompts.pkl'), 'rb'))
 
-# , , 'gpt2-xl', 'EleutherAI/gpt-neox-20b']:
-for checkpoint in checkpoints:
+for checkpoint in checkpoints_test:
     d = defaultdict(list)
     print('loading', checkpoint)
     model = prompt_classification.create_model(checkpoint)
     print('calculating accs...')
-    for task_name in tqdm(tasks):
-        for n_shots in [1, 5, 10]:
-            for prompt in ['', 'manual']:
+    for task_name in tqdm(settings['task_names']):
+        for n_shots in settings['n_shots']:
+            for prompt_type in settings['prompt_types']:
                 args.task_name = task_name
                 args.n_shots = n_shots
-                args.max_digit = max_digit
                 (dset, dset_test), check_answer_func, descr = data.get_data(
                     args, args.task_name, n_shots=args.n_shots,
                     train_split_frac=args.train_split_frac
                 )
                 d['checkpoint'].append(checkpoint)
-                d['prompt'].append(prompt)
+                d['prompt'].append(prompt_type)
                 d['task_name'].append(task_name)
                 d['n_shots'].append(n_shots)
-                if prompt == 'manual':
+                d['max_digit'].append(args.max_digit)
+                if prompt_type == 'manual':
                     prompt_actual = descr
-                else:
-                    prompt_actual = prompt
+                elif prompt_type in ['autoprompt', 'iprompt']:
+                    # get saved prompt
+                    prompt_actual = prompts_saved.loc[task_name][prompt_type]
+                elif prompt_type == '':
+                    prompt_actual = prompt_type
+
                 d['prompt_actual'].append(prompt_actual)
                 batch_size = batch_sizes.get(checkpoint, 16)
                 if task_name == 'task107_splash_question_to_sql':
@@ -101,10 +144,15 @@ for checkpoint in checkpoints:
                     batch_size=batch_size,
                 )
                 d['acc'].append(acc)
+
+            baseline_acc_dir = oj(results_acc_dir, 'baseline_accs')
+            ckpt = checkpoint.replace("/", "___")
             if task_key == 'one_digit_all':
-                save_name = f'results/generalization_acc/baseline_accs_{checkpoint.replace("/", "___")}___nshots={n_shots}.pkl'
+                save_name = oj(baseline_acc_dir, f'baseline_accs_{ckpt}___nshots={n_shots}.pkl')
             elif task_key == 'double_digit_two_nums':
-                save_name = f'results/generalization_acc/baseline_accs_{checkpoint.replace("/", "___")}___nshots={n_shots}___maxdigit={max_digit}.pkl'
+                save_name = oj(baseline_acc_dir, f'baseline_accs_{ckpt}___nshots={n_shots}___maxdigit={args.max_digit}.pkl')
             elif task_key == 'one_digit_three_nums':
-                save_name = f'results/generalization_acc/baseline_accs_{checkpoint.replace("/", "___")}___nshots={n_shots}___three_nums.pkl'
+                save_name = oj(baseline_acc_dir, f'baseline_accs_{ckpt}___nshots={n_shots}___three_nums.pkl')
+            elif task_key.startswith('sweep'):
+                save_name = oj(results_acc_dir, f'accs_sweep/accs_{ckpt}__{task_key}__{task_name}__prompt_type={prompt_type}.pkl')
             pkl.dump(d, open(save_name, 'wb'))
