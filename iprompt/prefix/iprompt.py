8feb0a5eedf6770df2b9328a4763b95889aeb8ad
Jack Morris <jxmorris12@gmail.com>
2022-11-19 01:24:26 -0500

    criterion=loss

--- iprompt/prefix/iprompt.py
+++ iprompt/prefix/iprompt.py
@@ -62,7 +62,7 @@ class iPrompt(AutoPrompt):
         ####################################################################
         self._prefix_pool = PrefixPool(
             tokenizer=self.tokenizer,
-            criterion='combined'  # in ['loss', 'acc', 'combined']
+            criterion='loss'  # in ['loss', 'acc', 'combined']
         )
         # Suff to track for early stopping
         self._last_population = None
