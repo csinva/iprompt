1b277c04337ff6b33d3f03f0d9b050416afcda42
Chandan Singh <chandan_singh@berkeley.edu>
2022-12-22 22:01:13 +0000

    try compound name in question

--- iprompt/data_utils/galactica.py
+++ iprompt/data_utils/galactica.py
@@ -11,15 +11,18 @@ GALACTICA_PROCESSED_DIR = oj(
 
 def fetch_bbbp():
     df = pd.read_csv(oj(GALACTICA_PROCESSED_DIR, 'bbbp.csv'))
+    df.name.loc[df.name.str.isnumeric()] = 'Compound-'+df.name.loc[df.name.str.isnumeric()] # rename compounds that are just numbers
     # Fix input: Encourage model to answer output as next token.
-    df['input'] = df['smiles'].map(lambda s: f'Here is a SMILES formula:\n[START_I_SMILES]{s}[END_I_SMILES]') + '\nAnswer:'
+    df['input'] = df['name'].map(lambda s: f'Here is a compound:\n{s}') + '\nAnswer:'
+    # df['input'] = df['smiles'].map(lambda s: f'Here is a compound:\n[START_I_SMILES]{s}[END_I_SMILES]') + '\nAnswer:'
     # Fix output: Prepend a space and add newlines to match output format of number tasks
+    # df['output'] = df['p_np'].map({0: 'NP', 1: 'P'}).map(lambda s: f' {s}\n\n')
     df['output'] = df['p_np'].map({0: 'No', 1: 'Yes'}).map(lambda s: f' {s}.\n\n')
     return df
 
 TASKS_GALACTICA = {
     'bbbp': {
-        'check_answer_func': r'blood|brain|barrier|permeability',
+        'check_answer_func': r'blood|brain|barrier|permeability|bbb',
         'description': 'Check if the compound permeates the blood-brain barrier',
         'gen_func': fetch_bbbp,
     }
