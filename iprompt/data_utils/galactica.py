6ea8af7ea93cd7213206611ab158a0e99c5a6066
Chandan Singh <chandan_singh@berkeley.edu>
2022-12-31 00:16:00 +0000

    basic cytoplasm example working

--- iprompt/data_utils/galactica.py
+++ iprompt/data_utils/galactica.py
@@ -4,6 +4,7 @@ import os
 import pandas as pd
 from os.path import dirname
 from os.path import join as oj
+import warnings
 
 GALACTICA_PROCESSED_DIR = oj(
     dirname(os.path.abspath(__file__)), 'galactica_raw')
@@ -46,6 +47,16 @@ def get_tox_dset(tox_target=0, return_tox_target_name=False):
     else:
         return df
 
+def load_uniprot(keyword1='Cytoplasm', keyword2='Membrane'):
+    df = pd.read_csv(oj(GALACTICA_PROCESSED_DIR, f'{keyword1}_{keyword2}.tsv'), sep='\t')
+    # df.name.loc[df.name.str.isnumeric()] = 'Compound-'+df.name.loc[df.name.str.isnumeric()] # rename compounds that are just numbers
+    # Fix input: Encourage model to answer output as next token.
+    df['input'] = df['Sequence'].map(lambda s: f'Here is a protein sequence:\n{s}') + '\nAnswer:'
+    # df['input'] = df['smiles'].map(lambda s: f'Here is a compound:\n[START_I_SMILES]{s}[END_I_SMILES]') + '\nAnswer:'
+    # Fix output: Prepend a space and add newlines to match output format of number tasks
+    df['output'] = df['key1'].map({0: 'Yes', 1: 'No'}).map(lambda s: f' {s}\n\n')
+    # df['output'] = df['p_np'].map({0: 'No', 1: 'Yes'}).map(lambda s: f' {s}.\n\n')
+    return df
 
 TASKS_GALACTICA = {
     'bbbp': {
@@ -53,9 +64,21 @@ TASKS_GALACTICA = {
         'description': 'Check if the compound permeates the blood-brain barrier',
         'gen_func': get_bbbp,
     },
+    'uniprot_cytoplasm_membrane': {
+        'check_answer_func': r'cytoplasm|membrane',
+        'description': 'Check if the protein has the keywor cytoplasm or membrane',
+        'gen_func': lambda: load_uniprot(keyword1='Cytoplasm', keyword2='Membrane'),
+    },
+    'uniprot_rna-binding_atp-binding': {
+        'check_answer_func': r'rna|atp',
+        'description': 'Check if the protein has the keywor rna-binding or atp-binding',
+        'gen_func': lambda: load_uniprot(keyword1='RNA-binding', keyword2='ATP-binding'),
+    }
 }
 for i in range(12):
-    df, tox_name = get_tox_dset(i, return_tox_target_name=True)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        df, tox_name = get_tox_dset(i, return_tox_target_name=True)
     TASKS_GALACTICA[f'tox21_{i}'] = {
         'check_answer_func': rf'{tox_name}',
         'description': f'Distinguish whether a compound is toxic to {tox_name}',
@@ -66,9 +89,11 @@ ks = list(TASKS_GALACTICA.keys())
 
 if __name__ == '__main__':
     print(TASKS_GALACTICA)
-    task = TASKS_GALACTICA['bbbp']
+    # task = TASKS_GALACTICA['bbbp']
+    task = TASKS_GALACTICA['uniprot_cytoplasm_membrane']
     df = task['gen_func']()
     print(repr(df.iloc[0]))
     print(repr(df.iloc[0]['input']))
     print(repr(df.iloc[0]['output']))
     print(df.head())
+    
