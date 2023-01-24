f38ebd40a910f8ae13e6a0e192079df26bb32b6d
Jack Morris <jxmorris12@gmail.com>
2023-01-24 15:49:47 -0500

    update ablation hparams

--- experiments/scripts/classification.py
+++ experiments/scripts/classification.py
@@ -5,7 +5,7 @@ import sys
 import submit_utils
 repo_dir = dirname(dirname(os.path.abspath(__file__)))
 
-save_dir = '/home/jxm3/research/prompting/interpretable-autoprompting/results_icml/classification_posneg'
+save_dir = '/home/jxm3/research/prompting/interpretable-autoprompting/results_icml/classification__prefix_after_input'
 
 cmd_python = 'python'
 
@@ -14,13 +14,10 @@ cmd_python = 'python'
 # --early_stopping_steps 100 --checkpoint EleutherAI/gpt-j-6B --batch_size 32 --float16 1 --n_shots=5 
 # --single_shot_loss=1   --mask_possible_answers=0   --task_name sst2_train
 PARAMS_SHARED_DICT = {
-    # things to vary
-    'mask_possible_answers': [0],
-    # iprompt_generation_repetition_penalty: [1.0, 1.5, 2.0],
-
     # stopping criteria
     'max_dset_size': [10_000], # sst2 has 10k sentences but could be more with a higher n_shots.
-    'num_learned_tokens': [12],
+    'num_learned_tokens': [6], # 
+    # 'num_learned_tokens': [12, 24],
     'task_name': [
         'sst2_train',
         'imdb_train',
@@ -31,13 +28,20 @@ PARAMS_SHARED_DICT = {
 
     'max_n_datapoints': [10_000],
     'early_stopping_steps': [100],
+    'model_cls': ['iprompt', 'autoprompt'],
 
     # fixed params
     'train_split_frac': [1.0],
     'single_shot_loss': [1],
     'n_shots': [1],
-    'seed': [3, 2, 1,],
+    'seed': [3, 2, 1],
     'max_length': [128],
+
+    # sentiment-specific args
+    'prefix_before_input': [0],
+    'mask_possible_answers': [1],
+    'iprompt_generation_repetition_penalty': [0.1], # ?
+    'iprompt_topk_strategy': ['all'],
 }
 PARAMS_SHARED_DICT['save_dir'] = [save_dir]
 
@@ -45,11 +49,6 @@ PARAMS_COUPLED_DICT = {  # these batch_sizes are roughly set for an A100 80GB gp
     ('checkpoint', 'batch_size', 'float16'): [
         ('EleutherAI/gpt-j-6B', 32, 1)
     ],
-    ('model_cls', 'iprompt_generation_repetition_penalty'): [
-        ('iprompt', 0.1),
-        ('iprompt', 1.0),
-        ('autoprompt', 1.0),
-    ]
     # ('task_name', 'iprompt_preprefix_str'): [
     #     ('sst2_train', "\"Answer Yes or No.\""),
     #     ('imdb_train', "\"Answer Yes or No.\""),
